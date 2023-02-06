d1559ce9c35ac3d023b96bdcd6db988a6d6cfc5b
maj <maximstomphorst@gmail.com>
2023-02-06 20:24:09 +0100

    new route

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace weatherapi.Controllers;
+
+[ApiController]
+[Route("/")]
+public class HomeController
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello max";
+    }
+}
